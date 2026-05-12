2df8d4b856689c32cb4ca8d0b91e2efa032c56af
Anthony Jean <lewazo@users.noreply.github.com>
2026-05-12 16:19:45 -0400

    Fix tests

--- test/changeset_parser_custom_field_constructor_test.exs
+++ test/changeset_parser_custom_field_constructor_test.exs
@@ -62,7 +62,9 @@ defmodule AbsintheErrorPayload.ChangesetParserCustomFieldConstructorTest do
   end
 
   setup do
+    original = Application.get_env(:absinthe_error_payload, :field_constructor)
     Application.put_env(:absinthe_error_payload, :field_constructor, CustomFieldConstructor)
+    on_exit(fn -> Application.put_env(:absinthe_error_payload, :field_constructor, original) end)
   end
 
   describe "construct_message/2" do
